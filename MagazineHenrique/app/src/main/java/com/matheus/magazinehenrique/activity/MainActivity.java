bdbbf84cf99c0a410e22366ec742a4496933c49e
Matheus Cunha Reis <matheuscunhareis30@gmail.com>
2017-10-08 12:38:14 -0300

    Updates

--- MagazineHenrique/app/src/main/java/com/matheus/magazinehenrique/activity/MainActivity.java
+++ MagazineHenrique/app/src/main/java/com/matheus/magazinehenrique/activity/MainActivity.java
@@ -1,13 +1,15 @@
-package com.matheus.magazinehenrique;
+package com.matheus.magazinehenrique.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import com.matheus.magazinehenrique.R;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
 }
